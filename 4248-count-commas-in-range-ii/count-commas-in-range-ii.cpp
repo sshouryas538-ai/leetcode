65e9f76c8facdf72e1b112007f1da8bd8edadf96
Shourya Sharma <sshouryas538@gmail.com>
2026-09-08 19:40:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (85.45%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
     long long countCommas(long long n) {
         long long ans = 0;
-        
         for (long long p = 1000; p <= n; p *= 1000) {
             ans += n - p + 1;
-            
-            // Prevent overflow
             if (p > n / 1000) break;
         }
-        
         return ans;
     }
 };
